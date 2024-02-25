4fde8188a2139f03cde48ac057707fe638ff9f49
Tatyana <tania-300@mail.ru>
2024-02-25 17:32:28 +0700

    header интерактивность

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navigation = document.querySelector('.header__navigation');
+const toggleButton = document.querySelector('.header__burger--closed');
+
+toggleButton.addEventListener('click', () => {
+  if (navigation.classList.contains('header__navigation--closed')) {
+    navigation.classList.remove('header__navigation--closed');
+    navigation.classList.add('header__navigation--opened');
+    toggleButton.classList.remove('header__burger--closed');
+  } else {
+    navigation.classList.remove('header__navigation--opened');
+    navigation.classList.add('header__navigation--closed');
+    toggleButton.classList.add('header__burger--closed');
+  }
+});
