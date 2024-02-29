347fe296a5d1384e5a7e7af502296a3b26935036
Tatyana <tania-300@mail.ru>
2024-02-29 22:17:50 +0700

    создает range слайдер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,7 @@
 /* в этот файл добавляет скрипты*/
 import { onToggleButtonClick, toggleButton } from './burger.js';
 import { showSlide } from './slider.js';
+import './range.js';
 
 toggleButton.addEventListener('click', onToggleButtonClick);
 
